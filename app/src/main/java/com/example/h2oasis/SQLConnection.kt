cb97f9572d63d86efbcd3edaa0912b6089e2c4b5
Osvaldo Ortega <osvaldo.ortega01@gmail.com>
2023-03-27 00:49:25 -0600

    Add connection string
    Added support for local host connection string, to query a local database. In this case is SQL Server, but later will change to a cloud-base storage solution. Anyway, it can now retrieve information of the database.

---
+++ app/src/main/java/com/example/h2oasis/SQLConnection.kt
@@ -0,0 +1,37 @@
+package com.example.h2oasis
+
+import android.os.StrictMode
+import android.util.Log
+import java.lang.Exception
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+class SQLConnection {
+    private val ip="192.168.1.23"
+    private val port="1433"
+    private val db="db_h2oasis"
+    private val username="test"
+    private val password="123"
+
+    fun dbConn(): Connection? {
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        var conn: Connection? = null
+        val connString : String
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance()
+            connString = "jdbc:jtds:sqlserver://" + ip + ":"+port+"/"+db;
+            //"encrypt=true;trustServerCertificate=false;loginTimeout=30;ssl=request"
+            conn = DriverManager.getConnection(connString, username, password)
+        } catch (ex: SQLException){
+            Log.e("Error: ", ex.message!!)
+        } catch (ex1: ClassNotFoundException)
+        {
+            Log.e("Error: ", ex1.message!!)
+        } catch (ex2: Exception){
+            Log.e("Error: ", ex2.message!!)
+        }
+        return conn
+    }
+}
